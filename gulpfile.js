8fe4192b23bae36a98325231b61f1a9fadef6454
Mykael Adam <mykael.adam@radio-canada.ca>
2017-06-20 15:18:57 -0400

    Inital commit - all the stuff

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,40 @@ var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
+var twig = require('gulp-twig');
 var cssGlobbing = require('gulp-css-globbing');
 var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync');
+var runSequence = require('run-sequence');
+var gutil = require('gulp-util');
+var watch = require('gulp-watch');
 
 var sass_config = {
     indentWidth: 4,
     outputStyle: 'compressed' ,
 };
+
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: 'dist'
+        },
+        ghostMode: false,
+        open: false
+    });
+});
+
+gulp.task('twig', function() {
+    return gulp.src(['twig/pages/**/*.twig'], {
+            base: 'twig/pages/'
+        })
+        .pipe(twig({
+            data: false ? require('twig/data.js') : {},
+            functions: '',
+            onError: function(event) {}
+        }))
+        .pipe(gulp.dest('dist/'));
+});
 
 gulp.task('styles',  function() {
     return gulp.src( 'sass/*.scss')
@@ -36,8 +63,43 @@ gulp.task('compress', function() {
     .pipe(gulp.dest('dist/js/'));
 });
 
-//Watch task
-gulp.task('default', ['styles', 'compress'], function() {
-    gulp.watch('sass/**/*.scss',['styles']);
-    gulp.watch('js/*.js',['compress']);
+gulp.task('copyJS', function() {
+  return gulp.src('js/**/*.js')
+    .pipe(gulp.dest('dist/js/'));
+});
+
+gulp.task('copyIMG', function() {
+  return gulp.src('img/**/')
+    .pipe(gulp.dest('dist/img/'));
+});
+
+// Default gulp taskgulp
+gulp.task('default', ['styles', 'compress', 'twig', 'copyJS', 'copyIMG', 'watch']);
+
+
+gulp.task('watch', ['browser-sync'], function() {
+    watch('twig/**/*.twig', function() {
+        runSequence('twig', function() {
+            browserSync.reload();
+            gutil.log(gutil.colors.green('Done T-rex TWIG!'));
+        });
+    });
+    watch('sass/**/*.scss', function() {
+         runSequence('styles', function() {
+            browserSync.reload();
+            gutil.log(gutil.colors.green('Done T-rex STYLES!'));
+        });
+    });
+    watch('js/*.js', function() {
+         runSequence('compress', 'copyJS', function() {
+            browserSync.reload();
+            gutil.log(gutil.colors.green('Done T-rex JS!'));
+        });
+    });
+    watch('img/**/', function() {
+         runSequence('copyIMG', function() {
+            browserSync.reload();
+            gutil.log(gutil.colors.green('Done T-rex IMG!'));
+        });
+    });
 });
